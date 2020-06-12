9955d86a17587378675592f367dcce0131c866c0
me <1912088450@qq.com>
2020-06-12 11:14:40 +0800

    调整输入格式，添加dropout

--- LSTMdemo.py
+++ LSTMdemo.py
@@ -25,17 +25,46 @@ def csv2df(file):
     df_original['timedelta_avr'] = df_original['time_delta'].mean()
     df_original['timestamp_max'] = df_original['timestamp'].max()
     df_original['timedelta_max'] = df_original['time_delta'].max()
-    df_original['tcp_num'] = df_original[df_original['proto'] == 6].shape[0]
-    df_original['udp_num'] = df_original[df_original['proto'] == 17].shape[0]
-    df_original['icmp_num'] = df_original[df_original['proto'] == 1].shape[0]
+    df_original['highest_layer_num'] = df_original['ip_highest_layer'].value_counts().shape[0]
+    # df_original['tcp_num'] = df_original[df_original['proto'] == 6].shape[0]
+    # df_original['udp_num'] = df_original[df_original['proto'] == 17].shape[0]
+    # df_original['icmp_num'] = df_original[df_original['proto'] == 1].shape[0]
     df_original['src_ip_num'] = df_original['src_ip'].value_counts().shape[0]
     df_original['dst_ip_num'] = df_original['dst_ip'].value_counts().shape[0]
     df_original['ip_ttl_avr'] = df_original['ip_ttl'].mean()
+    df_original['port1'] = df_original['src_port'].values[0]
+    df_original['port2'] = df_original['dst_port'].values[0]
+    df_original['ip1'] = df_original['src_ip'].values[0]
+    df_original['ip2'] = df_original['dst_ip'].values[0]
     df_new = df_original[["stream_index_avr", "packet_num", "timestamp_avr", "timedelta_avr", "timestamp_max", "timedelta_max",
-                        "tcp_num", "udp_num", "icmp_num", "src_ip_num", "dst_ip_num", "ip_ttl_avr", "label",
+                        "highest_layer_num", "ip_ttl_avr", "port1", "port2", "src_ip_num", "dst_ip_num", "label", 
                         "label0", "label1", "label2", "label3", "label4", "label5", "label6"]]
     df = df_new[0:1]
     return df
+
+# def csv2df(file):
+#     '''
+#     将单个整体csv样本文件转化为单行信息
+#     :return: 新转化的一个表示样本文件的dataframe，单行
+#     '''
+#     df_original = pd.DataFrame(pd.read_csv(file)).fillna(0)
+#     df_original['stream_index_avr'] = df_original['stream_index'].mean()
+#     df_original['packet_num'] = df_original.shape[0]
+#     df_original['timestamp_avr'] = df_original['timestamp'].mean()
+#     df_original['timedelta_avr'] = df_original['time_delta'].mean()
+#     df_original['timestamp_max'] = df_original['timestamp'].max()
+#     df_original['timedelta_max'] = df_original['time_delta'].max()
+#     df_original['tcp_num'] = df_original[df_original['proto'] == 6].shape[0]
+#     df_original['udp_num'] = df_original[df_original['proto'] == 17].shape[0]
+#     df_original['icmp_num'] = df_original[df_original['proto'] == 1].shape[0]
+#     df_original['src_ip_num'] = df_original['src_ip'].value_counts().shape[0]
+#     df_original['dst_ip_num'] = df_original['dst_ip'].value_counts().shape[0]
+#     df_original['ip_ttl_avr'] = df_original['ip_ttl'].mean()
+#     df_new = df_original[["stream_index_avr", "packet_num", "timestamp_avr", "timedelta_avr", "timestamp_max", "timedelta_max",
+#                         "tcp_num", "udp_num", "icmp_num", "src_ip_num", "dst_ip_num", "ip_ttl_avr", "label",
+#                         "label0", "label1", "label2", "label3", "label4", "label5", "label6"]]
+#     df = df_new[0:1]
+#     return df
 
 def traincsv2df(file):
     '''
@@ -86,10 +115,8 @@ def df2vec(dataframe):
     x_len_time = (x_len_time - x_len_time.min()) / (x_len_time.max() - x_len_time.min())
 
     # 可用pandas.get_dummies或sklearn进行one-hot
-    enc = OneHotEncoder(sparse=False).fit(dataframe[["tcp_num", "udp_num", "icmp_num", 
-                                     "src_ip_num", "dst_ip_num", "ip_ttl_avr"]])
-    X = np.concatenate([enc.transform(dataframe[["tcp_num", "udp_num", "icmp_num", 
-                                    "src_ip_num", "dst_ip_num", "ip_ttl_avr"]]),
+    enc = OneHotEncoder(sparse=False).fit(dataframe[["highest_layer_num", "ip_ttl_avr", "port1", "port2"]])
+    X = np.concatenate([enc.transform(dataframe[["highest_layer_num", "ip_ttl_avr", "port1", "port2"]]),
                         x_len_time], axis=1)
     return X
 
@@ -101,6 +128,27 @@ def create_dataset(dataset_X, dataset_Y, look_back=1):
         dataY.append(dataset_Y[i, :])
         # print(np.array(dataX, np.float16))
     return np.array(dataX, np.float16), np.array(dataY, np.float16)
+
+def metric_visualize(history, num):
+    # 绘制训练 & 验证的准确率值
+    plt.plot(history.history['accuracy'], label='train')
+    plt.plot(history.history['val_accuracy'], label='validation')
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.savefig(rootPath + "Accuracy" + str(num) + ".png")
+    plt.show()
+
+    # 绘制训练 & 验证的损失值
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='validation')
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.savefig(rootPath + "loss" + str(num) + ".png")
+    plt.show()
 
 def process_data(train, test, attr, lags):
     df1 = pd.read_csv(train, encoding='utf-8').fillna(0)
@@ -152,21 +200,25 @@ if __name__ == '__main__':
     lstmSecondLayer = 20
     outputLayer = 2 #输出层为包含2个分类的全连接层
     # seq_len = 30
-    seq_len = 10
-    batchSize = 512 #batch size
-    nb_epoch = 5 # 训练周期
+    seq_len = 1
+    batchSize = 32 #batch size
+    nb_epoch = 4 # 训练周期
 
     # 加载训练数据
-    rootPath = "D:\\PythonDemo\\LSTMdemo\\traindir\\"
+    rootPath = "D:\\PythonDemo\\LSTMdemo\\traindir_conversation\\"
     df = pd.DataFrame(columns=["stream_index_avr", "packet_num", "timestamp_avr", "timedelta_avr", "timestamp_max", "timedelta_max",
-                            "tcp_num", "udp_num", "icmp_num", "src_ip_num", "dst_ip_num", "ip_ttl_avr", "label",
-                            "label0", "label1", "label2", "label3", "label4", "label5", "label6"])
+                        "highest_layer_num", "ip_ttl_avr", "port1", "port2", "src_ip_num", "dst_ip_num" "label", 
+                        "label0", "label1", "label2", "label3", "label4", "label5", "label6"])
+    # df = pd.DataFrame(columns=["stream_index_avr", "packet_num", "timestamp_avr", "timedelta_avr", "timestamp_max", "timedelta_max",
+    #                         "tcp_num", "udp_num", "icmp_num", "src_ip_num", "dst_ip_num", "ip_ttl_avr", "label",
+    #                         "label0", "label1", "label2", "label3", "label4", "label5", "label6"])
     for root,dirs,files in os.walk(rootPath):
         for file in files:
             trainfile = os.path.join(root,file)
             traindf = csv2df(trainfile)
             df = df.append(traindf, ignore_index = True)
     # df = df.fillna(0)
+    print(df)
     print('> Loading data from: ' + root)
     
     # 某一个数据,可以使用pd.concat拼接多个dataframe，使用ignore_index=True重构索引
@@ -179,14 +231,15 @@ if __name__ == '__main__':
     accuracyList = []
 
     labelList = ["label0", "label1", "label2", "label3", "label4", "label5", "label6"]
-    for i in range(len(labelList)):
+    for i in range(1):
         labelNum = labelList[i]
         encY = OneHotEncoder(sparse=False).fit(df[[labelNum]])
         Y = encY.transform(df[labelNum].values.reshape(-1, 1))
-        train_x, val_x, train_y, val_y = train_test_split(X, Y, test_size=0.3, shuffle=True)
+        train_x, val_x, train_y, val_y = train_test_split(X, Y, test_size=0.25, shuffle=True)
         train_x, train_y = create_dataset(train_x, train_y, seq_len)
         val_x, val_y = create_dataset(val_x, val_y, seq_len)
         inputDim = train_x.shape[2]
+        print(inputDim)
         # 建模
         model = build_model([inputDim, lstmFirstLayer, lstmSecondLayer, outputLayer],seq_len)
         # 模型训练
@@ -196,13 +249,13 @@ if __name__ == '__main__':
         loss, accuracy = model.evaluate(val_x, val_y)
         lossList.append(loss)
         accuracyList.append(accuracy)
-        model.save('D:\\PythonDemo\\LSTMdemo\\model_new\\' + str(i) + '.h5')
+        model.save('D:\\PythonDemo\\LSTMdemo\\model_conversation\\' + str(i) + '.h5')
         print('Model ' + str(i) + " completed.", lossList[i])
 
         # 模型概括
         print("-----------------------model " + str(i))
         print(model.summary())
-        # metric_visualize(model_fit, i)
+        metric_visualize(model_fit, i)
         print("-----------------------")
 
     # 训练结果输出
